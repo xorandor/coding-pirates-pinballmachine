b258a5b729958d1277885cd30e96cb95638cd765
BuildTools <unconfigured@null.spigotmc.org>
2022-08-27 15:47:36 +0200

    add microbit sample

---
+++ communication trial/microbitsample.py
@@ -0,0 +1,11 @@
+#skal afvikles fra https://python.microbit.org/v/2
+from microbit import *
+
+while True:
+    display.scroll('A')
+    #display.show(Image.HEART)
+    sleep(1000)
+    print("POINT=3")
+    print("STATUS=enter")
+    print("STATUS=out")
+    print("LYD=A")
